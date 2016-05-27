1813bc47a7c5b008d1644c56c3dd90d73c53abee
tuannd255 <tuannd255@gmail.com>
2016-05-27 14:29:47 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  get 'application/hello'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
