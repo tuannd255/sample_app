f08ddf79aa3088b2ab1251394afea90ec902005d
tuannd255 <tuannd255@gmail.com>
2016-06-08 09:59:43 +0700

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
-	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+	before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        	:following, :followers]
 	before_action :correct_user,   only: [:edit, :update]
 	before_action :admin_user,     only: :destroy
 
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
   	end
 
   	def create
-  		params[:user][:sex] = params[:user][:sex].to_i
+  		#params[:user][:sex] = params[:user][:sex].to_i
 	    @user = User.new(user_params)
 	    if @user.save
 	    	@user.send_activation_email
@@ -52,6 +53,20 @@ class UsersController < ApplicationController
 	    flash[:success] = "User deleted"
 	    redirect_to users_url
 	  end
+
+	def following
+	    @title = "Following"
+	    @user  = User.find(params[:id])
+	    @users = @user.following.paginate(page: params[:page])
+	    render 'show_follow'
+	end
+
+	def followers
+	    @title = "Followers"
+	    @user  = User.find(params[:id])
+	    @users = @user.followers.paginate(page: params[:page])
+	    render 'show_follow'
+	end
 
   	private
 
